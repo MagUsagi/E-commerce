3910a65a9a3c8c583030ed64b65368bd9949b45e
Akane Klauenboesch <akane.klauenboesch@gmail.com>
2024-11-12 22:19:35 +0100

    built function to send purchase mail

--- payment/views.py
+++ payment/views.py
@@ -6,6 +6,10 @@ from cart.cart import Cart
 
 from django.http import JsonResponse
 
+from django.core.mail import send_mail
+
+from django.conf import settings
+
 # Create your views here.
 
 def checkout(request):
@@ -79,7 +83,14 @@ def complete_order(request):
             for item in cart:
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                                          price=item['price'], user=request.user)
-                
+
+
+            # Email order
+            send_mail('Order received', 'Hi! ' + '\n\n' + 'Thank you for placing your order' + '\n\n' +
+                      'Please see your order below' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: ' +
+                      str(cart.get_total()) + 'CHF', settings.EMAIL_HOST_USER, [email], fail_silently=False,)
+
+
         # 2) Create order -> Guest users without an account
         else:
             
@@ -88,10 +99,22 @@ def complete_order(request):
                 
             order_id = order.pk
 
+            product_list = []
+
             for item in cart:
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                                          price=item['price'])
         
+                product_list.append(item['product'])
+
+            all_products = product_list
+
+            # Email order
+            send_mail('Order received', 'Hi! ' + '\n\n' + 'Thank you for placing your order' + '\n\n' +
+                      'Please see your order below' + '\n\n' + str(all_products) + '\n\n' + 'Total paid: ' +
+                      str(cart.get_total()) + 'CHF', settings.EMAIL_HOST_USER, [email], fail_silently=False,)
+
+
         order_success = True
 
         response = JsonResponse({'success': order_success})
